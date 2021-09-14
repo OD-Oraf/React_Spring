bec1ce7a19c6cc7bc5ce65aec9856a8254a1bed6
OD-Oraf <43830269+OD-Oraf@users.noreply.github.com>
2021-09-14 00:18:56 -0400

    useEffect fetch students

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,26 +1,31 @@
-import {
-    Button,
-    Radio
-} from "antd";
+import { useState, useEffect } from 'react';
 import './App.css';
 
 import { getAllStudents } from "./client";
 
 function App() {
-    getAllStudents()
-        .then(res => res.json())
-        .then(console.log)
-    return (
-        <div className="App">
-            <Button type="primary">Hello</Button>
-            <br/>
-            <Radio.Group value='large'>
-                <Radio.Button value="large">Large</Radio.Button>
-                <Radio.Button value="default">Default</Radio.Button>
-                <Radio.Button value="small">Small</Radio.Button>
-            </Radio.Group>
-        </div>
-    );
+    const[students, setStudents] = useState([]);
+
+    const fetchStudents = () =>
+        getAllStudents()
+            //response sent back from api call
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                //Load student data into state
+                setStudents(data);
+            })
+
+
+    //when component loads call fetch students
+    useEffect(() => {
+        console.log("Component is mounted");
+        fetchStudents();
+    },[]);
+
+    return <p> {students.length} </p>;
+
+
 }
 
 export default App;
